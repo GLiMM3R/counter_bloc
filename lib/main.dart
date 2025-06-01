c10b8a3f418c5a1e1ab1064a821011b7dbccc30a
GLiMM3R <laykmk1997@gmail.com>
2025-06-01 13:42:01 +0700

    multiple bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:counter_bloc/bloc/counter_bloc.dart';
 import 'package:counter_bloc/counter_bloc_page.dart';
+import 'package:counter_bloc/counter_cubit_page.dart';
+import 'package:counter_bloc/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,18 +14,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Counter App",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CounterBloc()),
+        BlocProvider(create: (_) => CounterCubit()),
+      ],
+      child: MaterialApp(
+        title: "Counter App",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: BlocProvider(
-        //   create: (_) => CounterCubit(),
-        //   child: const CounterCubitPage(),
-        create: (_) => CounterBloc(),
-        child: const CounterBlocPage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter App")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CounterBlocPage(),
+                  ),
+                );
+              },
+              child: const Text("Counter Bloc"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CounterCubitPage(),
+                  ),
+                );
+              },
+              child: const Text("Counter Cubit"),
+            ),
+          ],
+        ),
       ),
     );
   }
